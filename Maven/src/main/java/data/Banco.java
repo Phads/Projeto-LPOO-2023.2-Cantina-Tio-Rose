94bf3d016e0ba993463760b61dfb50cfed8191f9
Pedro Alves Six <alvessilva9168@gmail.com>
2023-11-22 15:35:48 -0300

    Commit de testes para italo

---
+++ Maven/src/main/java/data/Banco.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Banco {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Maven/src/main/java/gui/Telas.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class Telas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
